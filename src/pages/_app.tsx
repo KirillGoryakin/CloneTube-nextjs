58f56d7a5ed755293d37c009e9304ae682dc2564
KirillGoryakin <kirillgoryakin.dev@gmail.com>
2023-02-01 19:39:15 +0500

    Add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import { Layout } from '@/components/Layout';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import 'src/styles/global.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>CloneTube</title>
+      </Head>
+      
       <Component {...pageProps} />
     </Layout>
   );
